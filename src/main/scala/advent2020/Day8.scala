80fc3019e38e9185bb64ab10c5da6ee6b2bd9970
Prajwal Tuladhar <praj@infynyxx.com>
2021-02-28 14:13:17 -0500

    day8 part2

--- src/main/scala/advent2020/Day8.scala
+++ src/main/scala/advent2020/Day8.scala
@@ -4,7 +4,7 @@ object Day8 {
   def main(args: Array[String]) {
     val lines = inputToVector("input_day8.txt");
     println(
-      pixelate(lines.head)
+      pixelatePart2(lines.head)
     )
   }
 
@@ -15,21 +15,22 @@ object Day8 {
       .map(_.toVector)
       .toVector
 
-    val x = partitioned.map(elem => elem.filter(char => char != '0').size)
+    val x = partitioned
+      .map(elem => elem.filter(char => char != '0').size)
       .zipWithIndex
-      .toArray.sortWith(_._1 > _._1)
+      .toArray
+      .sortWith(_._1 > _._1)
       .toVector
     val layerIndex = x.head._2
     val layer = partitioned(layerIndex)
-    val oneAndTwoDigits = layer.foldLeft((0, 0)) {
-      (a, char) =>
-        if (char == '1') {
-          (a._1 + 1, a._2)
-        } else if (char == '2') {
-          (a._1, a._2 + 1)
-        } else {
-          (a._1, a._2)
-        }
+    val oneAndTwoDigits = layer.foldLeft((0, 0)) { (a, char) =>
+      if (char == '1') {
+        (a._1 + 1, a._2)
+      } else if (char == '2') {
+        (a._1, a._2 + 1)
+      } else {
+        (a._1, a._2)
+      }
     }
     println(layer)
     println(oneAndTwoDigits)
@@ -37,5 +38,32 @@ object Day8 {
   }
 
   def pixelatePart2(input: String) = {
+    /**
+      * 01
+      * 10
+      */
+     val partitioned = input
+      .toCharArray()
+      .grouped(25 * 6)
+      .map(_.toVector)
+      .toVector
+    println(partitioned)
+    val x = partitioned.foldLeft(Vector.empty[Char]) { (vec, arr) =>
+      if (vec.isEmpty) {
+        arr
+      } else {
+        vec.zipWithIndex.map({
+          case(item, index) => {
+            if (item == '1' || item == '0') {
+              item
+            } else {
+              arr(index)
+            }
+          }
+        }).toVector
+      }
+    }
+    println(x)
+    println(x.mkString(""))
   }
 }
